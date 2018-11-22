8d7dc0c4f37e81fa8e79f2206ecd34c60c0a0de1
DenilsonDesigns <daniellanguilller@gmail.com>
2018-11-22 12:59:16 +1100

    begin cors issues

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+import * as actions from "../../actions/index";
+import { compose } from "redux";
 
 class Signup extends Component {
+  onSubmit = formProps => {
+    this.props.signup(formProps);
+  };
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit)}>
         <fieldset>
           <label htmlFor="">Email</label>
           <Field
@@ -23,9 +32,16 @@ class Signup extends Component {
             autoComplete="off"
           />
         </fieldset>
+        <button>Sign Up!</button>
       </form>
     );
   }
 }
 
-export default reduxForm({ form: "signup" })(Signup);
+export default compose(
+  connect(
+    null,
+    actions
+  ),
+  reduxForm({ form: "signup" })
+)(Signup);
